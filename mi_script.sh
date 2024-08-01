9955594000a55757e0a93d0827b41b160bce4f69
facu145 <facucastrokoch@gmail.com>
2024-08-01 17:19:05 -0300

    Update mi_script.sh

--- mi_script.sh
+++ mi_script.sh
@@ -1,1 +1,20 @@
-echo "hello world"
+#!/bin/bash
+
+mkdir -p /home/linux/rad
+cd /home/linux/rad
+echo 'echo "hello world"' > mi_script.sh
+chmod +x mi_script.sh
+
+tmp_crontab=$(mktemp)
+
+crontab -l > "$tmp_crontab"
+
+echo "0 9 * * * /home/linux/rad/mi_script.sh" >> "$tmp_crontab"
+echo "30 20 * * * /home/linux/rad/mi_script.sh" >> "$tmp_crontab"
+echo "0 18 * * 0 /home/linux/rad/mi_script.sh" >> "$tmp_crontab"
+
+crontab "$tmp_crontab"
+
+rm "$tmp_crontab"
+
+echo "Crontab entries created successfully."
